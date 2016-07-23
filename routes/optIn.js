d363469289ca942943d4b8576b5ffa1bb5a63ba6
ityn <ityn8161@gmail.com>
2016-07-24 01:06:50 +0700

    home

--- routes/optIn.js
+++ routes/optIn.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.post('/', function(req, res, next) {
     res.status(200).send('Acceptable');
-	console.log(req.body);
+	console.log('_____ jr'); //(req.body);
 });
 
 module.exports = router;
