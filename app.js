8c401896fa7a64ae2abd8a4b2264cead044fa560
ityn <ityn@github.com>
2016-07-24 00:07:56 +0600

    node_modules/

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var optIn = require('./routes/optIn');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/opt', optIn);
 app.use('/', routes);
 app.use('/users', users);
 
